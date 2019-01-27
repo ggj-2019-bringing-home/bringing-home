b7506a56a772030096f8f3aabb4204a3fca01e99
mah <mathias.haimerl@ese.de>
2019-01-27 12:36:26 +0100

    Added base title card

---
+++ src/helpers/SoundHandler.js
@@ -0,0 +1,10 @@
+export default class SoundHandler {
+    constructor(scene) {
+        this.scene = scene;
+    }
+    preload() {
+        for (let i = 1; i <= 6; i++) {
+            this.scene.
+        }
+    }
+}
--- src/scenes/IntroScene.js
+++ src/scenes/IntroScene.js
@@ -6,6 +6,23 @@ export default class IntroScene extends Phaser.Scene {
     }
 
     preload() {
+        this.types = [
+            'amphibians',
+            'bird',
+            'carnivore',
+            'coral',
+            'flowers',
+            'herbivore',
+            'herbs',
+            'omnivore',
+            'seaweed',
+            'weed'
+        ];
+        this.icons = [
+            'rect',
+            'tick'
+        ];
+        this.load.atlas('icons', 'assets/icons.png', 'assets/icons.json');
         this.headings = [
             'Level one : Once upon a time',
             'Level two : Green hands',
@@ -14,39 +31,40 @@ export default class IntroScene extends Phaser.Scene {
         this.quests = [
             [
                 'Get some water!\n',
-                'Create a planet with stone and water.\n'
+                'Create a planet with stone and water.'
             ],
             [
-                'Get some plants!',
+                'Get some plants!\n',
                 'FLOWERS and HERBS > WEED'
             ],
             [
-                'Get some animals!',
-                'Keep enough plants, even with animals',
+                'Get some animals!\n',
+                'Keep enough plants, even with animals\n',
                 'HERBIVORE > BIRDS'
             ]
         ];
         this.desciptions = [
-            [],
-            [
-                'HERB like cold weather.',
-                'FLOWERS like hot weather.',
-                'WEED don\'t like to be with herb and it really love eating FLOWERS.',
-                'SEAWEED are eating CORAL.',
-                'CORAL are eating SEAWEED and hates\ncold weather.'
-            ],
-            [
-                'HERBIVORE are eating the HERB and FLOWERS.',
-                'OMNIVORE are eating WEED and CARNIVORE.',
-                'CARNIVORE are eating BIRDS and Herbivore.',
-                'BIRDS are eating AMPHIBIANS.',
-                'AMPHIBIANS are eating Coral and Omnivore.'
-            ]
+            {},
+            {
+                herbs: 'HERB like cold weather.\n',
+                flowers: 'FLOWERS like hot weather.\n',
+                weed: 'WEED don\'t like to be with herb\nand it really love eating FLOWERS.',
+                seaweed: 'SEAWEED are eating CORAL.\n',
+                coral: 'CORAL are eating SEAWEED and\nhates cold weather.'
+            },
+            {
+                herbivore: 'HERBIVORE are eating the HERB and FLOWERS.\n',
+                omnivore: 'OMNIVORE are eating WEED and CARNIVORE.\n',
+                carnivore: 'CARNIVORE are eating BIRDS and Herbivore.\n',
+                birds: 'BIRDS are eating AMPHIBIANS.\n',
+                amphibians: 'AMPHIBIANS are eating Coral and Omnivore.\n'
+            }
         ];
     }
 
     create(data) {
         this.levelIdx = data.level - 1;
+        const card = this;
         // Background
         const gfx = this.add.graphics();
         gfx.fillStyle(0x1E1B1B, 1);
@@ -55,17 +73,23 @@ export default class IntroScene extends Phaser.Scene {
         // Texts
         this.add.text(
             this.game.config.width / 6,
-            this.game.config.height / 6,
+            100,
             this.headings[this.levelIdx],
             {
-                font: '36px Verdana',
+                font: '40px Verdana',
                 fill: '#D5C8C8'
             }
         );
+        let i = 0;
         this.add.text(
             this.game.config.width / 4,
             (this.game.config.height / 6) + 75,
             this.quests[this.levelIdx].reduce((res, cur) => {
+                const imgPos = (card.game.config.height / 6) + 92 + (i++ * 74);
+                card.add.image(
+                    (this.game.config.width / 4) - 40,
+                    imgPos, 'icons', 'rect'
+                );
                 res += `${cur}\n`;
                 return res;
             }, ''),
@@ -75,19 +99,38 @@ export default class IntroScene extends Phaser.Scene {
             }
         );
         // Todo Add boxes
-        this.add.text(
-            this.game.config.width / 6,
-            3 * (this.game.config.height / 5),
-            this.desciptions[this.levelIdx].slice(0, 2).reduce((s, c) => `${s}${c}\n\n`, ''),
-            {
-                font: '30px Verdana',
-                fill: '#D5C8C8'
-            }
-        );
+        i = 0;
+        let x = 60;
+        Object.keys(this.desciptions[this.levelIdx]).slice(0, 3).forEach(c => {
+            card.add.text(
+                x + 40,
+                2 * (card.game.config.height / 5) + (++i * 118),
+                card.desciptions[this.levelIdx][c],
+                {
+                    font: '24px Verdana',
+                    fill: '#D5C8C8'
+                }
+            );
+            card.add.image(x, 2.5 * (card.game.config.height / 5) + (i * 118) - 60, 'icons', c);
+        });
+        i = 0;
+        x += this.game.config.width / 2;
+        Object.keys(this.desciptions[this.levelIdx]).slice(3).forEach(c => {
+            card.add.text(
+                x + 40,
+                2 * (card.game.config.height / 5) + (++i * 118),
+                card.desciptions[this.levelIdx][c],
+                {
+                    font: '24px Verdana',
+                    fill: '#D5C8C8'
+                }
+            );
+            card.add.image(x, 2.5 * (card.game.config.height / 5) + (i * 118) - 60, 'icons', c);
+        });
         // Todo Add Images
 
         this.input.once('pointerdown', function (event) {
-            this.scene.start('PlanetScene');
+            this.scene.start('PlanetScene', {level: data.level});
             this.scene.start('UiScene');
         }, this);
     }
